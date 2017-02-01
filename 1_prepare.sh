91e7b48a90290224ea753c6b41e84307fffff32b
Zhang <jun283@users.noreply.github.com>
2017-02-01 10:45:05 +0800

    Update 1_prepare.sh

--- 1_prepare.sh
+++ 1_prepare.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#/usr/bin/yum -y install yum-utils cmake patch make gcc gcc-c++  flex bison file libtool libtool-libs autoconf kernel-devel gd gd-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glib2 glib2-devel bzip2 bzip2-devel libevent libevent-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel  gettext gettext-devel gmp-devel pspell-devel unzip   mail sysstat net-snmp net-snmp-utils libxslt mtr iotop  'libpcap*' 'bind-utils' 'dnsutils' 'libedit*' 'net-snmp-*' 'libtidy*' 'libxml*'  'libxsl*'  'icu*' 'libicu*' 'libcurl*' 'libtidy*'  'tracer*' bc ntp httpd-devel apr apr-util-devel apr-devel
-
 #/bin/yum -y install  gcc gcc-c++ cmake  pcre pcre-devel  zlib zlib-devel  openssl openssl-devel  
 #/bin/yum -y install kernel-devel gd gd-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glib2 glib2-devel bzip2 bzip2-devel libevent libevent-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel  gettext gettext-devel gmp-devel pspell-devel unzip   mail sysstat net-snmp net-snmp-utils libxslt mtr iotop  'libpcap*' 'bind-utils' 'dnsutils' 'libedit*' 'net-snmp-*' 'libtidy*' 'libxml*'  'libxsl*'  'icu*' 'libicu*' 'libcurl*' 'libtidy*'  'tracer*' bc ntp httpd-devel apr apr-util-devel apr-devel 'libtool*' autoconf xz-devel python-devel httpd  
 
