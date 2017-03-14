f94b26b2fabb4cfb9c7d6871d03c79f4062a3782
Zhang <jun283@users.noreply.github.com>
2017-03-14 11:37:56 +0800

    Update 3_build.sh

--- 3_build.sh
+++ 3_build.sh
@@ -60,38 +60,20 @@ cd ${BUILD_SRC}nginx-1.10.2
 --add-module=${BUILD_SRC}modsecurity-2.9.1/nginx/modsecurity/ \
 --add-module=${BUILD_SRC}testcookie-nginx-module-master 
 
-#--with-mail=dynamic \
-#--with-http_perl_module \
-#--pid-path=${NGINX_PATH}sbin/nginx.pid \
-#--error-log-path=$DEFV_SoftProgram_PATH"nginx/logs/error.log" 
-#--http-log-path=$DEFV_SoftProgram_PATH"nginx/logs/access.log" 
-#--http-client-body-temp-path=$DEFV_SoftProgram_PATH"nginx/client_body_temp" 
-#--http-proxy-temp-path=$DEFV_SoftProgram_PATH"nginx/temp/" 
-#--http-fastcgi-temp-path=$DEFV_SoftProgram_PATH"nginx/fastcgi_temp" 
-#--http-uwsgi-temp-path=$DEFV_SoftProgram_PATH"nginx/uwsgi_temp" 
-#--http-scgi-temp-path=$DEFV_SoftProgram_PATH"nginx/uwsgi_temp" 
-#--sbin-path=/usr/local/nginx/sbin/nginx \
-#--conf-path=/usr/local/nginx/sbin/nginx/nginx.conf \
-#--sbin-path=${NGINX_PATH}sbin/nginx \
-#--conf-path=${NGINX_PATH}conf/nginx.conf
-#>> ${BUILD}build.log
-#--with-http_geoip_module \
-#--add-module=/usr/build/nginx-rtmp-module \
-#--add-dynamic-module=/usr/build/3party_module \
 
 make
 make install
 
 #create serve
-#cp nginx.service /lib/systemd/system
+cp nginx.service /lib/systemd/system
 
 #start nginx
-#systemctl start nginx.service
+systemctl start nginx.service
 #start ngiinx when OS start
-#systemctl enable nginx.service
+systemctl enable nginx.service
 #check state
-#systemctl status nginx.service
-#systemctl stop nginx
+systemctl status nginx.service
+
 
 
 
