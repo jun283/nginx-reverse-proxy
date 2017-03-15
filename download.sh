f98808abfa649621a41276593cdc0bee1886a974
Zhang <jun283@users.noreply.github.com>
2017-03-15 14:19:41 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#ftp download site
-#http://www.filewatcher.com/
+
 . colors.sh
 
 chmod u+x *.sh 
@@ -25,38 +24,40 @@ rm -rf ${DOWNLOAD}/*
 cd ${DOWNLOAD}
 
 show_blue_bg "Download" "GeoIP.tar.gz"
-wget -q --no-check-certificate http://geolite.maxmind.com/download/geoip/api/c/GeoIP.tar.gz
+#wget -q --no-check-certificate http://geolite.maxmind.com/download/geoip/api/c/GeoIP.tar.gz
 tar  -zxf GeoIP.tar.gz -C $BUILD_SRC
 
 
 show_blue_bg "Download" "apr-1.5.2.tar.gz"
-wget -q --no-check-certificate http://www-us.apache.org/dist//apr/apr-1.5.2.tar.gz
+#wget -q --no-check-certificate http://www-us.apache.org/dist//apr/apr-1.5.2.tar.gz
 tar  -zxf apr-1.5.2.tar.gz -C $BUILD_SRC
 
 show_blue_bg "Download" "apr-util-1.5.4.tar.gz"
-wget -q --no-check-certificate http://www-us.apache.org/dist//apr/apr-util-1.5.4.tar.gz #http://download.openpkg.org/components/cache/apr/apr-util-1.5.4.tar.gz
+#wget -q --no-check-certificate http://www-us.apache.org/dist//apr/apr-util-1.5.4.tar.gz 
 tar  -zxf apr-util-1.5.4.tar.gz -C $BUILD_SRC
 
 
 show_blue_bg "Download" "nginx-upstream-fair"
-wget -q --no-check-certificate  https://github.com/gnosek/nginx-upstream-fair/tarball/master -O nginx-upstream-fair.tar.gz
+#wget -q --no-check-certificate  https://github.com/gnosek/nginx-upstream-fair/tarball/master -O nginx-upstream-fair.tar.gz
 tar  -zxf nginx-upstream-fair.tar.gz -C $BUILD_SRC
 #unzip -q -o "nginx-upstream-fair.zip" -d $BUILD
 
 show_blue_bg "Download" "ngx_cache_purge-2.3"
-wget -q --no-check-certificate http://labs.frickle.com/files/ngx_cache_purge-2.3.tar.gz
+#wget -q --no-check-certificate http://labs.frickle.com/files/ngx_cache_purge-2.3.tar.gz
 tar  -zxf ngx_cache_purge-2.3.tar.gz -C $BUILD_SRC
 
 show_blue_bg "Download" "testcookie"
-wget -q --no-check-certificate https://github.com/kyprizel/testcookie-nginx-module/archive/master.zip -O testcookie.zip
-unzip -q -o "testcookie.zip" -d $BUILD_SRC
+#wget -q --no-check-certificate https://github.com/kyprizel/testcookie-nginx-module/archive/master.zip -O testcookie.zip
+#unzip -q -o "testcookie.zip" -d $BUILD_SRC
+#wget -q --no-check-certificate  https://github.com/kyprizel/testcookie-nginx-module/tarball/master -O testcookie-v1.2.tar.gz
+tar  -zxf testcookie-v1.2.tar.gz -C $BUILD_SRC
 
 show_blue_bg "GIT" "git clone https://github.com/SpiderLabs/owasp-modsecurity-crs"
 cd ${NGINX_PATH}conf
 git clone https://github.com/SpiderLabs/owasp-modsecurity-crs
 
 show_blue_bg "Download" "nginx-1.10.2.tar.gz"
-wget -q --no-check-certificate http://nginx.org/download/nginx-1.10.2.tar.gz  #--no-same-owner 
+#wget -q --no-check-certificate http://nginx.org/download/nginx-1.10.2.tar.gz  #--no-same-owner 
 if [ -s "nginx-1.10.2.tar.gz" ] ; then
 	run_ok
     tar  -zxf nginx-1.10.2.tar.gz -C $BUILD_SRC
@@ -65,23 +66,18 @@ else
 fi
 
 show_blue_bg "Download" "pcre-8.39"
-wget  --no-check-certificate ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.gz
+#wget  --no-check-certificate ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.gz
 tar   -xzf pcre-8.39.tar.gz -C $BUILD_SRC
 
 show_blue_bg "Download" "zlib-1.2.11"
-wget -q --no-check-certificate http://zlib.net/zlib-1.2.11.tar.gz
+#wget -q --no-check-certificate http://zlib.net/zlib-1.2.11.tar.gz
 tar  -xzf zlib-1.2.11.tar.gz -C $BUILD_SRC
 
 show_blue_bg "Download" "openssl-1.0.2j.tar.gz"
-wget -q --no-check-certificate http://www.openssl.org/source/openssl-1.0.2j.tar.gz
+#wget -q --no-check-certificate http://www.openssl.org/source/openssl-1.0.2j.tar.gz
 tar  -xzf openssl-1.0.2j.tar.gz -C $BUILD_SRC
 
 show_blue_bg "Download" "modsecurity-2.9.1.tar.gz"
-wget -q --no-check-certificate https://www.modsecurity.org/tarball/2.9.1/modsecurity-2.9.1.tar.gz
+#wget -q --no-check-certificate https://www.modsecurity.org/tarball/2.9.1/modsecurity-2.9.1.tar.gz
 tar  -xzf modsecurity-2.9.1.tar.gz -C $BUILD_SRC
 
-#change chmod 755
-#find $BUILD_SRC -type d -exec chmod 755 {} \;
-cd $BUILD_SRC
-chmod 755 ./ -R
-chown -R root.root ./
